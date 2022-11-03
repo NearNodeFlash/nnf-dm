cbc7d1f5b4f376b5f29964108d0efa54568ec63b
Nate Roiger <nate.roiger@hpe.com>
2022-11-03 08:22:26 -0500

    Remove nnf-dm specific prefix from service arguments; this will permit us to use more common variable names between daemons.
    Signed-off-by: Nate Roiger <nate.roiger@hpe.com>

--- daemons/compute/server/servers/server.go
+++ daemons/compute/server/servers/server.go
@@ -52,8 +52,8 @@ func GetOptions() (*ServerOptions, error) {
 	flag.StringVar(&opts.port, "kubernetes-service-port", opts.port, "Kubernetes service port number")
 	flag.StringVar(&opts.name, "node-name", opts.name, "Name of this compute resource")
 	flag.StringVar(&opts.nodeName, "nnf-node-name", opts.nodeName, "NNF node name that should handle the data movement request")
-	flag.StringVar(&opts.tokenFile, "nnf-data-movement-service-token-file", opts.tokenFile, "Path to the NNF data movement service token")
-	flag.StringVar(&opts.certFile, "nnf-data-movement-service-cert-file", opts.certFile, "Path to the NNF data movement service certificate")
+	flag.StringVar(&opts.tokenFile, "service-token-file", opts.tokenFile, "Path to the NNF data movement service token")
+	flag.StringVar(&opts.certFile, "service-cert-file", opts.certFile, "Path to the NNF data movement service certificate")
 	flag.BoolVar(&opts.simulated, "simulated", opts.simulated, "Run in simulation mode where no requests are sent to the server")
 	flag.Parse()
 	return &opts, nil
