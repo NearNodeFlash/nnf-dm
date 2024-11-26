90a51f773cd4cbcb9699e10eeeddd8ea243b0adf
Dean Roehrich <dean.roehrich@hpe.com>
2024-11-26 10:14:19 -0600

    fix for test
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- internal/controller/datamovementmanager_controller.go
+++ internal/controller/datamovementmanager_controller.go
@@ -461,11 +461,15 @@ func (r *NnfDataMovementManagerReconciler) createOrUpdateDaemonSetIfNecessary(ct
 		podSpec.NodeSelector = manager.Spec.Selector.MatchLabels
 		podSpec.Subdomain = serviceName
 
+		_, isTest := os.LookupEnv("NNF_TEST_ENVIRONMENT")
 		container, err := findManagerContainer(podSpec)
-		if err != nil {
+		// The test env doesn't build the full pod spec, so it may not have
+		// this container.
+		if err != nil && !isTest {
 			return err
+		} else if err == nil {
+			container.Env = append(container.Env, corev1.EnvVar{Name: "ENVIRONMENT", Value: os.Getenv("ENVIRONMENT")})
 		}
-		container.Env = append(container.Env, corev1.EnvVar{Name: "ENVIRONMENT", Value: os.Getenv("ENVIRONMENT")})
 
 		setupSSHAuthVolumes(manager, podSpec)
 
