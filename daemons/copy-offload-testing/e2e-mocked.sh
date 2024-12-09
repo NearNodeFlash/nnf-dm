7e6fedbd25718b725734e7705541ab895095052a
Dean Roehrich <dean.roehrich@hpe.com>
2024-12-09 11:29:51 -0600

    Add libcopyoffload and an end2end test for mock mode (#240)
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

---
+++ daemons/copy-offload-testing/e2e-mocked.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Copyright 2024 Hewlett Packard Enterprise Development LP
+# Other additional copyright holders may be indicated within.
+#
+# The entirety of this work is licensed under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -o pipefail
+
+make build-copy-offload-local
+
+make -C ./daemons/lib-copy-offload tester
+CO=./daemons/lib-copy-offload/tester
+
+NNF_NODE_NAME=rabbit01 ./bin/nnf-copy-offload -port 4000 -mock &
+srvr_pid=$!
+echo "Server pid is $srvr_pid, my pid is $$"
+
+trap cleanup SIGINT SIGQUIT SIGABRT SIGTERM
+# shellcheck disable=SC2317
+cleanup() {
+    echo "FAIL: trap"
+    if [[ -n $srvr_pid ]]; then
+        kill "$srvr_pid"
+    fi
+    exit 1
+}
+
+echo "Waiting for daemon to start"
+while : ; do
+    sleep 1
+    if curl http://0.0.0.0:4000/hello 2> /dev/null; then
+        break
+    fi
+done
+
+output=$($CO -l 0.0.0.0:4000)
+if [[ $output != "" ]]; then
+    echo "FAIL: Expected empty output from list before any jobs have been submitted"
+    kill "$srvr_pid"
+    exit 1
+fi
+
+output=$($CO -c nnf-copy-offload-node-2 0.0.0.0:4000)
+if [[ $output != "" ]]; then
+    echo "FAIL: Expected empty output from cancel before any jobs have been submitted"
+    kill "$srvr_pid"
+    exit 1
+fi
+
+job1=$($CO -o -C compute-01 -W yellow -S /mnt/nnf/ooo -D /lus/foo 0.0.0.0:4000)
+if [[ $job1 != "nnf-copy-offload-node-0" ]]; then
+    echo "FAIL: Unexpected output from copy. Got ($job1)."
+    kill "$srvr_pid"
+    exit 1
+fi
+
+output=$($CO -l 0.0.0.0:4000)
+if [[ $(echo "$output" | wc -l) -ne 1 ]]; then
+    echo "FAIL: Unexpected output from list. Got ($output)."
+    kill "$srvr_pid"
+    exit 1
+fi
+
+output=$($CO -c "$job1" 0.0.0.0:4000)
+if [[ $output != "" ]]; then
+    echo "FAIL: Expected empty output from cancel $job1"
+    kill "$srvr_pid"
+    exit 1
+fi
+
+output=$($CO -l 0.0.0.0:4000)
+if [[ $output != "" ]]; then
+    echo "FAIL: Expected empty output from list after all jobs have been removed"
+    kill "$srvr_pid"
+    exit 1
+fi
+
+echo "Kill server $srvr_pid"
+kill "$srvr_pid"
+echo "PASS: Success"
+exit 0
+
