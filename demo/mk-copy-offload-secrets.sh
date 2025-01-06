ff658d239d3723c0496e78f05926c764bf44adac
Dean Roehrich <dean.roehrich@hpe.com>
2025-01-06 09:11:56 -0600

    demo crumbs
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

---
+++ demo/mk-copy-offload-secrets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2025 Hewlett Packard Enterprise Development LP
+# Other additional copyright holders may be indicated within.
+#
+# The entirety of this work is licensed under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -o pipefail
+
+CERTDIR=daemons/copy-offload/certs
+./daemons/copy-offload-testing/gen_certs.sh $CERTDIR
+
+KEY=$CERTDIR/ca/private/ca_key.pem
+SERVER_CERT=$CERTDIR/server/server_cert.pem
+TOKEN=$CERTDIR/client/token
+
+SERVER_SECRET=nnf-dm-copy-offload-server
+kubectl create secret tls $SERVER_SECRET --cert $SERVER_CERT --key $KEY
+
+TOKEN_SECRET=nnf-dm-copy-offload-token
+kubectl create secret generic $TOKEN_SECRET --from-file token="$TOKEN"
--- demo/mk-secret.sh
+++ demo/mk-secret.sh
@@ -6,9 +6,10 @@ set -o pipefail
 
 CERTDIR=certs
 
+KEY=$CERTDIR/ca/private/ca_key.pem
 SERVER_CERT=$CERTDIR/server/server_cert.pem
 CLIENT_CERT=$CERTDIR/client/client_cert.pem
-KEY=$CERTDIR/ca/private/ca_key.pem
+TOKEN=$CERTDIR/client/token
 
 SERVER_SECRET=nnf-dm-copy-offload-server
 kubectl create secret tls $SERVER_SECRET --cert $SERVER_CERT --key $KEY
@@ -17,3 +18,6 @@ if [[ -n $MTLS ]]; then
     CLIENT_SECRET=nnf-dm-copy-offload-client
     kubectl create secret tls $CLIENT_SECRET --cert $CLIENT_CERT --key $KEY
 fi
+
+TOKEN_SECRET=nnf-dm-copy-offload-token
+kubectl create secret generic $TOKEN_SECRET --from-file token="$TOKEN"
