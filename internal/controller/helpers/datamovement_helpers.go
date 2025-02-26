297f1a2282e6823549297ecea85d179af6d1b7aa
Blake Devcich <blake.devcich@hpe.com>
2025-02-26 10:27:39 -0600

    Use $SETPRIV in mkdir/stat command when necessary
    With the new copy offload container, the container is ran as the
    specified user, so setpriv commands are not necessary to preserve the
    proper permissions. However, for DataIn/DataOut DM, data movement is
    still performed as root in the nnf-dm containers. Setpriv is necessary
    in this case.
    
    This change determines the UID of the user and appropriate uses/drops
    the setprivCommand defined by the DM profile when issuing mkdir and stat
    commands.
    
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- internal/controller/helpers/datamovement_helpers.go
+++ internal/controller/helpers/datamovement_helpers.go
@@ -298,7 +298,15 @@ func BuildDMCommand(profile *nnfv1alpha6.NnfDataMovementProfile, hostfile string
 	return strings.Split(cmd, " "), nil
 }
 
-func buildStatOrMkdirCommand(uid, gid uint32, cmd, hostfile, path string) string {
+func buildStatOrMkdirCommand(uid, gid uint32, cmd, setprivCmd, hostfile, path string) string {
+
+	// For DataIn/DataOut, dm is ran as root, so setpriv is needed to become the specified user. For
+	// copy offload, the user container is ran as the user, so setpriv is not needed.
+	if os.Getuid() != 0 {
+		setprivCmd = ""
+	}
+	cmd = strings.ReplaceAll(cmd, "$SETPRIV", setprivCmd)
+
 	cmd = strings.ReplaceAll(cmd, "$HOSTFILE", hostfile)
 	cmd = strings.ReplaceAll(cmd, "$UID", fmt.Sprintf("%d", uid))
 	cmd = strings.ReplaceAll(cmd, "$GID", fmt.Sprintf("%d", gid))
@@ -483,7 +491,7 @@ func GetDestinationDir(profile *nnfv1alpha6.NnfDataMovementProfile, dm *nnfv1alp
 
 // Use mpirun to run stat on a file as a given UID/GID by using `setpriv`
 func mpiStat(profile *nnfv1alpha6.NnfDataMovementProfile, path string, uid, gid uint32, mpiHostfile string, log logr.Logger) (string, error) {
-	cmd := buildStatOrMkdirCommand(uid, gid, profile.Data.StatCommand, mpiHostfile, path)
+	cmd := buildStatOrMkdirCommand(uid, gid, profile.Data.StatCommand, profile.Data.SetprivCommand, mpiHostfile, path)
 
 	output, err := command.Run(cmd, log)
 	if err != nil {
@@ -595,7 +603,7 @@ func createDestinationDir(profile *nnfv1alpha6.NnfDataMovementProfile, dm *nnfv1
 		return nil
 	}
 
-	cmd := buildStatOrMkdirCommand(dm.Spec.UserId, dm.Spec.GroupId, profile.Data.MkdirCommand, mpiHostfile, dest)
+	cmd := buildStatOrMkdirCommand(dm.Spec.UserId, dm.Spec.GroupId, profile.Data.MkdirCommand, profile.Data.SetprivCommand, mpiHostfile, dest)
 	output, err := command.Run(cmd, log)
 	if err != nil {
 		return fmt.Errorf("data movement mkdir failed ('%s'): %w output: %s", cmd, err, output)
